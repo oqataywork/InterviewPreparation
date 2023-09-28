e008069eacf6dc83c9f1f0a071fa32de2783220e
oqataywork <ogtayaliyev1@gmail.com>
2023-09-28 23:16:26 +0300

    +AnonMethods

---
+++ AnonMethods/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace AnonMethods
+{
+
+    public delegate int AddDelegate(int a, int b);
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            AddDelegate mD = new AddDelegate(delegate (int num1, int num2) { return num1 + num2; });
+            Console.WriteLine(mD.Invoke(5, 7));
+             // md();
+            Console.ReadKey();
+        }
+    }
+}
